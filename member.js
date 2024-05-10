ad110357e3e3a0ba73bc95c6162545a632011734
bruno-rodriguessilva <bruno.rodriguessilva@capgemini.com>
2024-05-10 15:14:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    member.style.display = "none";
+    skills.style.display = "block";
+    console.log("skillsMember");
+}
